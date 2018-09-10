22f7c229310662107cebe79807cc2bb6f3ef09ea
rodrigo <rodrigo.aze7@gmail.com>
2018-09-10 10:55:27 -0300

    Adicionando providers de delete e get de rodadas

---
+++ src/providers/administrador/rodadas/deleteRodada.js
@@ -0,0 +1,17 @@
+import variables from '../../../variables';
+import axios from 'axios';
+import utilLocalStorage from '../../../util/localStorage';
+
+const removerRodada = async (data) => {
+    let dataResponse;
+    let token = utilLocalStorage.getToken();
+    try{
+        dataResponse = await axios.delete(`${variables.urlApi}/administrador/rodadas`, {data: {id: id, token: token}});
+    }catch(error){
+        dataResponse = error.response.data;
+    }
+    
+    return dataResponse;
+}
+
+export default { removerRodada };
---
+++ src/providers/administrador/rodadas/listarRodada.js
@@ -0,0 +1,17 @@
+import variables from '../../../variables';
+import axios from 'axios';
+import utilLocalStorage from '../../../util/localStorage';
+
+const listarRodadas = async (data) => {
+    let dataResponse;
+    let token = utilLocalStorage.getToken();
+    try{
+        dataResponse = await axios.get(`${variables.urlApi}/administrador/rodadas/${token}`);
+    }catch(error){
+        dataResponse = error.response.data;
+    }
+    
+    return dataResponse;
+}
+
+export default { listarRodadas };
