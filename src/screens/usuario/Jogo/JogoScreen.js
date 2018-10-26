b49149af3f7c467ce5b5d84dcf693963f10b869f
rodrigo <rodrigo.aze7@gmail.com>
2018-10-26 20:28:03 -0300

    gif de loading para rodadas

--- src/screens/usuario/Jogo/JogoScreen.js
+++ src/screens/usuario/Jogo/JogoScreen.js
@@ -16,28 +16,29 @@ export default class JogoScreen extends Component {
             respostaSelecionada: "",
             tempoQuestao: 30,
             completions: 0,
-            questao:[],
-            perguntasRespondidas: 0
+            questao: [],
+            perguntasRespondidas: 0,
+            loading: false
         }
     }
-    gerarNovaQuestao = async () =>{
-        
+    gerarNovaQuestao = async () => {
+
         let idQuiz;
-        if(localStorage.getItem('idQuizAtivo')!==undefined) idQuiz = localStorage.getItem('idQuizAtivo');
+        if (localStorage.getItem('idQuizAtivo') !== undefined) idQuiz = localStorage.getItem('idQuizAtivo');
         let usuario = utilUser.getUser();
-        let requestQuestao = await providerQuestaoAleatoria.questaoAleatoria({idQuiz: idQuiz, idUsuario: usuario._id});
-        if(requestQuestao.data!==undefined && requestQuestao.data.status && !requestQuestao.data.finalizado){
+        let requestQuestao = await providerQuestaoAleatoria.questaoAleatoria({ idQuiz: idQuiz, idUsuario: usuario._id });
+        if (requestQuestao.data !== undefined && requestQuestao.data.status && !requestQuestao.data.finalizado) {
             await this.setState({
-                questao:[
+                questao: [
                     requestQuestao.data.questao
                 ]
             });
-        }else if(requestQuestao.finalizado){
+        } else if (requestQuestao.finalizado) {
             let questoes = [];
-            if(localStorage.getItem('jogoDescifre')!==null) questoes = JSON.parse(localStorage.getItem('jogoDescifre'));
-            const requestProcessarQuiz = await providerProcessarQuiz.processarQuiz({questoes, idQuiz});
+            if (localStorage.getItem('jogoDescifre') !== null) questoes = JSON.parse(localStorage.getItem('jogoDescifre'));
+            const requestProcessarQuiz = await providerProcessarQuiz.processarQuiz({ questoes, idQuiz });
             localStorage.setItem('idRodadaEntrar', requestProcessarQuiz.data.idRodada);
-            localStorage.setItem('resultadoQuiz',JSON.stringify(requestProcessarQuiz.data.resultado));
+            localStorage.setItem('resultadoQuiz', JSON.stringify(requestProcessarQuiz.data.resultado));
             browserHistory.push('/usuario/resultados');
             window.location.reload();
         }
@@ -47,11 +48,12 @@ export default class JogoScreen extends Component {
         document.title = "Jogo usuário - A melhor platahtmlForma de entreterimento do Brasil.";
         await this.gerarNovaQuestao()
     }
-    handleResposta = async (e) =>{
+    handleResposta = async (e) => {
         await this.setState({
+            loading: true,
             respostaSelecionada: e.target.value
         });
-        if(this.state.respostaSelecionada===null || this.state.respostaSelecionada===""){
+        if (this.state.respostaSelecionada === null || this.state.respostaSelecionada === "") {
             Swal({
                 type: 'error',
                 title: 'Oops...',
@@ -63,35 +65,37 @@ export default class JogoScreen extends Component {
         setTimeout(async () => {
             // Array de jogadas
             let jogadas = [];
-            if(localStorage.getItem('jogoDescifre')!==null) jogadas = JSON.parse(localStorage.getItem('jogoDescifre'));
-            
+            if (localStorage.getItem('jogoDescifre') !== null) jogadas = JSON.parse(localStorage.getItem('jogoDescifre'));
+
             let item = {
                 idQuestao: this.state.questao[0]._id,
                 alternativa: this.state.respostaSelecionada
             }
-            
-            if(!jogadas.find(x => x.idQuestao === this.state.questao[0]._id)){
+
+            if (!jogadas.find(x => x.idQuestao === this.state.questao[0]._id)) {
                 jogadas.push(item);
                 localStorage.setItem('jogoDescifre', JSON.stringify(jogadas));
                 await this.setState({
-                    respostaSelecionada:null,
+                    respostaSelecionada: null,
                     perguntasRespondidas: jogadas.length
                 });
             }
             await this.gerarNovaQuestao();
             await this.setState({
                 tempoQuestao: 30,
-                completions: this.state.completions + 1
-            });  
-            window.scrollTo(0,0);
-        },200);
+                completions: this.state.completions + 1,
+                loading: false
+            });
+            window.scrollTo(0, 0);
+        }, 200);
     }
+
     handleNovaQuestao = async () => {
         await this.gerarNovaQuestao();
         await this.setState({
             tempoQuestao: 30,
             completions: this.state.completions + 1
-        });    
+        });
     }
     // Aprovar possível remoção
     // handleClick = async (e) => {
@@ -108,12 +112,12 @@ export default class JogoScreen extends Component {
     //     // Array de jogadas
     //     let jogadas = [];
     //     if(localStorage.getItem('jogoDescifre')!==null) jogadas = JSON.parse(localStorage.getItem('jogoDescifre'));
-        
+
     //     let item = {
     //         idQuestao: this.state.questao[0]._id,
     //         alternativa: this.state.respostaSelecionada
     //     }
-        
+
     //     if(!jogadas.find(x => x.idQuestao === this.state.questao[0]._id)){
     //         jogadas.push(item);
     //         localStorage.setItem('jogoDescifre', JSON.stringify(jogadas));
@@ -131,30 +135,32 @@ export default class JogoScreen extends Component {
     // }
 
     handleClickPular = async (e) => {
+        this.setState({loading: true})
         e.preventDefault();
         // Array de jogadas
         let jogadas = [];
-        if(localStorage.getItem('jogoDescifre')!==null) jogadas = JSON.parse(localStorage.getItem('jogoDescifre'));
-        
+        if (localStorage.getItem('jogoDescifre') !== null) jogadas = JSON.parse(localStorage.getItem('jogoDescifre'));
+
         let item = {
             idQuestao: this.state.questao[0]._id,
             alternativa: null
         }
-        
-        if(!jogadas.find(x => x.idQuestao === this.state.questao[0]._id)){
+
+        if (!jogadas.find(x => x.idQuestao === this.state.questao[0]._id)) {
             jogadas.push(item);
             localStorage.setItem('jogoDescifre', JSON.stringify(jogadas));
             await this.setState({
-                respostaSelecionada:null,
+                respostaSelecionada: null,
                 perguntasRespondidas: jogadas.length
             });
         }
         await this.gerarNovaQuestao();
         await this.setState({
             tempoQuestao: 30,
-            completions: this.state.completions + 1
-        });  
-        window.scrollTo(0,0);
+            completions: this.state.completions + 1,
+            loading: false
+        });
+        window.scrollTo(0, 0);
     }
 
     render() {
@@ -183,70 +189,79 @@ export default class JogoScreen extends Component {
                         <div className="row justify-content-center">
                             <div className="col-lg-12">
                                 <div className="card bg-secondary shadow border-0">
-                                    <div className="card-body px-lg-5 py-lg-5">
-                                        <div className="row">
-                                            <div className="col-6">
+                                    {!this.state.loading &&
+                                        <div className="card-body px-lg-5 py-lg-5">
+                                            <div className="row">
+                                                <div className="col-6">
 
-                                                <center>
-                                                    <h4 style={{ fontSize: '50px', color: '#212121', fontWeight: '400' }}>
-                                                        {this.state.perguntasRespondidas}
-                                                    </h4>
-                                                    <h5 style={{ fontSize: '16px', color: '#212121', fontWeight: '400' }}>
-                                                        PERGUNTAS RESPONDIDAS
+                                                    <center>
+                                                        <h4 style={{ fontSize: '50px', color: '#212121', fontWeight: '400' }}>
+                                                            {this.state.perguntasRespondidas}
+                                                        </h4>
+                                                        <h5 style={{ fontSize: '16px', color: '#212121', fontWeight: '400' }}>
+                                                            PERGUNTAS RESPONDIDAS
                                                     </h5>
-                                                </center>
-                  
-                                            </div>
-                                            <div className="col-6">
-                                                <div className="offset-1 col-6">
-                                                    <ReactCountdownClock 
-                                                    key={this.state.completions}
-                                                    seconds={this.state.tempoQuestao}
-                                                    color="#212121"
-                                                    alpha={1}
-                                                    size={100}
-                                                    onComplete={this.handleNovaQuestao}
-                                                    />
+                                                    </center>
+
+                                                </div>
+                                                <div className="col-6">
+                                                    <div className="offset-1 col-6">
+                                                        <ReactCountdownClock
+                                                            key={this.state.completions}
+                                                            seconds={this.state.tempoQuestao}
+                                                            color="#212121"
+                                                            alpha={1}
+                                                            size={100}
+                                                            onComplete={this.handleNovaQuestao}
+                                                        />
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <hr/>
-                                        <div className="row">
-                                            {this.state.questao.map((questao, index)=>{
-                                                return(
-                                                    <div key={index} className="col-lg-12">
-                                                        <p style={{ fontSize: '16px', color: '#212121', fontWeight: '400' }}>({questao.categoria.nome}) - {questao.enunciado}</p>
 
-                                                        <div onChange={e=>this.handleResposta(e)}>
-                                                            {
-                                                                questao.alternativas.map((alternativa, indexAlternativa)=>{
-                                                                    return(
-                                                                        <div className="custom-control custom-radio mb-3 radio" key={indexAlternativa}>
-                                                                            <input name="resposta" className="custom-control-input" id={`alternativa${indexAlternativa}`} type="radio" value={alternativa.descricao}
-                                                                            checked={this.state.respostaSelecionada === alternativa.descricao}
-                                                                            />
-                                                                            <label className="custom-control-label" htmlFor={`alternativa${indexAlternativa}`}>
-                                                                                <span>{alternativa.descricao}</span>
-                                                                            </label>
-                                                                        </div>
-                                                                    )
-                                                                })
-                                                            }
+                                            <hr />
+                                            <div className="row">
+                                                {this.state.questao.map((questao, index) => {
+                                                    return (
+                                                        <div key={index} className="col-lg-12">
+                                                            <p style={{ fontSize: '16px', color: '#212121', fontWeight: '400' }}>({questao.categoria.nome}) - {questao.enunciado}</p>
+
+                                                            <div onChange={e => this.handleResposta(e)}>
+                                                                {
+                                                                    questao.alternativas.map((alternativa, indexAlternativa) => {
+                                                                        return (
+                                                                            <div className="custom-control custom-radio mb-3 radio" key={indexAlternativa}>
+                                                                                <input name="resposta" className="custom-control-input" id={`alternativa${indexAlternativa}`} type="radio" value={alternativa.descricao}
+                                                                                    checked={this.state.respostaSelecionada === alternativa.descricao}
+                                                                                />
+                                                                                <label className="custom-control-label" htmlFor={`alternativa${indexAlternativa}`}>
+                                                                                    <span>{alternativa.descricao}</span>
+                                                                                </label>
+                                                                            </div>
+                                                                        )
+                                                                    })
+                                                                }
+                                                            </div>
                                                         </div>
-                                                    </div>
 
-                                                )
-                                            })}
-                                            
+                                                    )
+                                                })}
 
-                                        </div>
-                                        <hr/>
-                                        <div className="row">
-                                            <div className="col-lg-12">
-                                                <button onClick={this.handleClickPular} type="button" className="btn btn-block btn-danger">Pular <i className="fas fa-forward" style={{"color":"#ffffff"}}></i></button>
+
                                             </div>
+                                            <hr />
+                                            <div className="row">
+                                                <div className="col-lg-12">
+                                                    <button onClick={this.handleClickPular} type="button" className="btn btn-block btn-danger">Pular <i className="fas fa-forward" style={{ "color": "#ffffff" }}></i></button>
+                                                </div>
+                                            </div>
+
                                         </div>
-                                    </div>
+                                    }
+                                    {this.state.loading &&
+                                        <center>
+                                            <img className="img-fluid" alt="Menino triste" src="/img/public/loading.gif" />
+                                        </center>
+                                    }
                                 </div>
                                 <br />
                                 <br />
