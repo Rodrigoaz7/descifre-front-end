c8b892c0f8944720c3ed0ff7fd1eef9fcb091d33
rodrigo <rodrigo.aze7@gmail.com>
2018-11-09 09:18:18 -0300

    Problema de responsividade em tabela de classificacao

--- src/screens/usuario/Classificacao/ClassificacaoScreen.js
+++ src/screens/usuario/Classificacao/ClassificacaoScreen.js
@@ -9,8 +9,8 @@ export default class ClassificacaoScreen extends Component {
     constructor() {
         super();
         this.state = {
-           classificacao: [],
-           colocacao: 0 
+            classificacao: [],
+            colocacao: 0
         }
     }
 
@@ -18,16 +18,16 @@ export default class ClassificacaoScreen extends Component {
         let idRodada = this.props.params.idRodada;
         const rodada = await providerObterRodada.obterRodada(idRodada);
         let usuario = utilUsuario.getUser();
-        let colocacao = rodada.data.rodadas.jogadores.findIndex(jogada => jogada.quiz.idUsuario===usuario._id);
+        let colocacao = rodada.data.rodadas.jogadores.findIndex(jogada => jogada.quiz.idUsuario === usuario._id);
 
         await this.setState({
             classificacao: rodada.data.rodadas.jogadores,
-            colocacao: colocacao+1,
+            colocacao: colocacao + 1,
             ganhadoresTamanho: rodada.data.rodadas.ganhadores.length
         });
         document.title = `Classificação da rodada - De$cifre, muito mais do que um jogo.`;
     }
-    
+
     render() {
         return (
             <div className="position-relative alt">
@@ -66,10 +66,10 @@ export default class ClassificacaoScreen extends Component {
                                                         JOGADORES
                                                     </h5>
                                                 </center>
-                  
+
                                             </div>
                                             <div className="col-6">
-                                                
+
                                                 <center>
                                                     <h4 style={{ fontSize: '50px', color: '#212121', fontWeight: '400' }}>
                                                         {this.state.colocacao}º
@@ -78,61 +78,62 @@ export default class ClassificacaoScreen extends Component {
                                                         SUA COLOCAÇÃO
                                                     </h5>
                                                 </center>
-                                                
+
                                             </div>
                                         </div>
-                                        <hr/>
+                                        <hr />
                                         <div className="row">
                                             <div className="col-lg-12">
                                                 <center>
                                                     <h5 style={{ fontSize: '22px', color: '#212121', fontWeight: '400' }}>
                                                         CLASSIFICAÇÃO GERAL
                                                     </h5>
-                                                    <span style={{fontSize:'12px'}}>(Jogadores com fundo verde estão ganhando cifras nessa rodada)</span>
+                                                    <span style={{ fontSize: '12px' }}>(Jogadores com fundo verde estão ganhando cifras nessa rodada)</span>
                                                 </center>
-                                                <br/>
-                                                <table className="table">
-                                                    <thead>
-                                                        <tr>
-                                                            <th scope="col">#</th>
-                                                            <th scope="col">
-                                                                <center>
-                                                                    Nome
+                                                <br />
+                                                <div className="table-responsive">
+                                                    <table className="table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">#</th>
+                                                                <th scope="col">
+                                                                    <center>
+                                                                        Nome
                                                                 </center>
-                                                            </th>
-                                                            <th scope="col">
-                                                                <center>
-                                                                    Pontuação
+                                                                </th>
+                                                                <th scope="col">
+                                                                    <center>
+                                                                        Pontuação
                                                                 </center>
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {
-                                                            this.state.classificacao.map((jogador, index)=>{
-                                                                return(
-                                                                    <tr key={index} bgcolor={index<this.state.ganhadoresTamanho?"#DCEDC8":"#FFFFFF"}>
-                                                                        <th scope="row">{index+1}</th>
-                                                                        <td>
-                                                                            <center>
-                                                                                {jogador.quiz.nomeUsuario}
-                                                                            </center>
-                                                                        </td>
-                                                                        <td>
-                                                                            <center>
-                                                                                {
-                                                                                    jogador.quiz.pontuacao
-                                                                                }
-                                                                                
-                                                                            </center>
-                                                                        </td>
-                                                                    </tr>
-                                                                )
-                                                            })
-                                                        }
-                                                    </tbody>
-                                                </table>
-                                               
+                                                                </th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            {
+                                                                this.state.classificacao.map((jogador, index) => {
+                                                                    return (
+                                                                        <tr key={index} bgcolor={index < this.state.ganhadoresTamanho ? "#DCEDC8" : "#FFFFFF"}>
+                                                                            <th scope="row">{index + 1}</th>
+                                                                            <td>
+                                                                                <center>
+                                                                                    {jogador.quiz.nomeUsuario}
+                                                                                </center>
+                                                                            </td>
+                                                                            <td>
+                                                                                <center>
+                                                                                    {
+                                                                                        jogador.quiz.pontuacao
+                                                                                    }
+
+                                                                                </center>
+                                                                            </td>
+                                                                        </tr>
+                                                                    )
+                                                                })
+                                                            }
+                                                        </tbody>
+                                                    </table>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
