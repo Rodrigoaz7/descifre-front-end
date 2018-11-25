9285845aaf0163a5d6cb095e2ce5a279a9fcb108
Marcus Dantas <mpsdantas15@gmail.com>
2018-11-25 11:13:54 -0300

    Prod lomadee.

--- src/ui/components/Lomadee.js
+++ src/ui/components/Lomadee.js
@@ -16,11 +16,9 @@ export default class Lomadee extends Component {
                     <div className="card-body px-lg-5 py-lg-5">
                         <div className="row">
                             <div className="col-lg-12">
-                            
-                            
-                            <div class="g3S6Z4x927jE" style={{width: '728px', height: '90px'}}>
-                                <script type="text/javascript" class="lomadee-recommender-script" src="//ad.lomadee.com/v1/eyJwdWJsaXNoZXJJZCI6MjI3ODQ1MTQsInNpdGVJZCI6MzM5NDc3NDIsInNvdXJjZUlkIjozNjAyNzI3OX0%3D.js?w=728&h=90"></script>
-                            </div>
+                                <div className="g3S6Z4x927jE" style={{width: '728px', height: '90px'}}>
+                                    <script type="text/javascript" className="lomadee-recommender-script" src="//ad.lomadee.com/v1/eyJwdWJsaXNoZXJJZCI6MjI3ODQ1MTQsInNpdGVJZCI6MzM5NDc3NDIsInNvdXJjZUlkIjozNjAyNzI3OX0%3D.js?w=728&h=90"></script>
+                                </div>
 
                             </div>
                         </div>
